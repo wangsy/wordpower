8c8bd17e9096d664d0c7346926778cc90d333a83
Sooyong Wang <wangsy@wangsy.com>
2011-11-24 00:09:25 +0900

    add other files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordpower::Application.config.secret_token = '6a81f6f505e6818f6e16978c192e6f32a8635ef9ac45284642001937ef4ae274496f111e8ea4485339e482c0cf7869bc7e8bb4b61e0a4e26b120b24ba5be22d4'
